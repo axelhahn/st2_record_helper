2077b82010f7fd43b09b013d37406a8cb32782e2
Axel Hahn <ax2002@gmx.net>
2022-11-14 15:24:18 +0100

    remove file in tests subdir

--- tests/urls_2_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-declare -a sUrls=(
-    http://www.surfmusik.de/m3u/station/radio-fritz,965.m3u  # follow "location:" + read m3u --> audio/mpeg icecast stream
-    http://uk2.internet-radio.com:8121/listen.pls
-)
